4845b15d7735009b94c30491af3d312d87843e92
mohammadAmir <m.amir.b1389@gmail.com>
2025-03-09 19:23:56 +0330

    index/

--- pages/index.vue
+++ pages/index.vue
@@ -4,11 +4,15 @@
 
 <template>
   
- <main>
+ <main class="p-10">
   
-  <p class="light">heeelllooo
-  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae, in repudiandae excepturi rem ea quod quasi placeat optio, quaerat minus possimus laborum perspiciatis at eligendi blanditiis officia natus adipisci tenetur.
+  <p class="light text-2xl  ">سلاام
+  <span class="text-9xl">
+   خوش اومدی
+  </span>
 </p>
+<br>
+<NuxtLink class="underline hover:text-blue-400" to="/blog">بیا توی بلاگم>>>>>>></NuxtLink>
     
  </main>
 </template>

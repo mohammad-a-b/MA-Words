db7c35f20b032a290122894c80289398a68e4644
mohammadAmir <m.amir.b1389@gmail.com>
2025-05-06 18:23:40 +0330

    fix comment

--- components/Comments.vue
+++ components/Comments.vue
@@ -5,7 +5,7 @@ import { useToast } from 'vue-toastification'
 const toast = useToast()
 const { $supabase } = useNuxtApp()
 
-const comments = ref([])
+const   comments = ref([])
 const loading = ref(true)
 const submitting = ref(false)
 
